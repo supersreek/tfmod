60c84cc9c431796b8074a508536b87c820435f34
Sreek <ksreek1171@gmail.com>
2018-07-02 14:01:21 -0400

    adding new tf files to repo

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable modname {}
+variable SubnetId {
+  default = "subnet-07287a63"
+  description = "The subnet id of the default vpc in the working region"
+}
+variable SecGroupId {
+  description = "this variable does not have a default value but is passed from the main code"
+  }
